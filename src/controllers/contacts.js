9ecbd45e3bea2afff2ba1227c7871dab630a74a0
Yana17-ratsa <ratsayana13@gmail.com>
2024-11-01 14:14:58 +0200

    hw 3 ready for checking

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,11 @@
-import { getContacts, getContactById } from '../services/contacts.js';
+import {
+  getContacts,
+  getContactById,
+  postContact,
+  deleteContact,
+} from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
 
 export const getContactsController = async (_, res, next) => {
   try {
@@ -16,10 +22,13 @@ export const getContactsController = async (_, res, next) => {
 
 export const getContactsByIdController = async (req, res) => {
   const { _id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    throw createHttpError(404, 'Contact not found');
+  }
   const data = await getContactById(_id);
 
   if (!data) {
-    throw createHttpError(404, `Contact with id=${_id} not found!`);
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.json({
@@ -27,4 +36,27 @@ export const getContactsByIdController = async (req, res) => {
     message: 'Successfully found contact!',
     data,
   });
+};
+
+export const postContactController = async (req, res) => {
+  const student = await postContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: student,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  const contact = await deleteContact(contactId);
+
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(204).send();
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import {
+  deleteContactController,
   getContactsByIdController,
   getContactsController,
+  postContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -10,5 +12,9 @@ const router = Router();
 router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:_id', ctrlWrapper(getContactsByIdController));
+
+router.post('/contacts', ctrlWrapper(postContactController));
+
+router.delete('contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
