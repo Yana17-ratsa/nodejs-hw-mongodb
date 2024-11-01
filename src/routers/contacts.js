92c1bbc237be80fe14ba3aa452b1de9858a93aaa
Yana17-ratsa <ratsayana13@gmail.com>
2024-11-01 12:29:52 +0200

    2 steps finished

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,36 +1,14 @@
-import {Router} from 'express';
-import { getContactById, getContacts } from '../services/contacts.js';
+import { Router } from 'express';
+import {
+  getContactsByIdController,
+  getContactsController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
-router.get('/contacts', async (_, res) => {
-    const data = await getContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data,
-    });
-  });
+router.get('/contacts', ctrlWrapper(getContactsController));
 
-
-
-  router.get("/contacts/:_id", async (req, res) => {
-    const { _id } = req.params;
-    const data = await getContactById(_id);
-
-    if (!data) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with id=${_id} not found!`,
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: 'Successfully found contact!',
-      data,
-    });
-  });
-
+router.get('/contacts/:_id', ctrlWrapper(getContactsByIdController));
 
 export default router;
