71a9873479e6a0d40621a2ac6ccca62f52c454ed
Yana17-ratsa <ratsayana13@gmail.com>
2024-12-02 13:44:08 +0200

    update controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,7 +7,7 @@ import { logoutUser } from '../services/auth.js';
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
-  res.starus(201).json({
+  res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
     data: user,
