88d89394d83d8b5ced5270a0d5dc2905923cad11
Yana17-ratsa <ratsayana13@gmail.com>
2024-12-05 14:08:47 +0200

    resolved all issues

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,10 @@
-import { isValidObjectId } from "mongoose";
-import createHttpError from "http-errors";
+import { isValidObjectId } from 'mongoose';
+import createHttpError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-    const {_id} = req.params;
-    if(!isValidObjectId(_id)) {
-        throw createHttpError(400, 'Bad request');
-    }
-    next();
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    throw createHttpError(400, 'Bad request');
+  }
+  next();
 };
