caf494bac1f8856f2d364937b6379f2a858c28bd
Yana17-ratsa <ratsayana13@gmail.com>
2024-12-06 12:00:01 +0200

    hw started

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,6 +10,9 @@ import mongoose from 'mongoose';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
+// import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+// import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+// import { env } from '../utils/env.js';
 
 export const getContactsController = async (req, res, next) => {
   try {
@@ -71,12 +74,29 @@ export const postContactController = async (req, res) => {
   });
 };
 
+
+
+
 export const patchContactController = async (req, res, next) => {
   const { id } = req.params;
   const { _id: userId } = req.user;
-  console.log(id, userId);
 
-  const result = await updateContact(id, req.body, userId);
+    // const photo = req.file;
+
+  // let photoUrl;
+
+  // if (photo) {
+  //   if (env('ENABLE_CLOUDINARY' === 'true')) {
+  //     photoUrl = await saveFileToCloudinary(photo);
+  //   } else {
+  //     photoUrl = await saveFileToUploadDir(photo);
+  //   }
+  // }
+  const result = await updateContact(id, userId, {
+    ...req.body,
+    // photo: photoUrl,
+  });
+
 
   if (!result) {
     next(createHttpError(404, 'Contact not found!'));
